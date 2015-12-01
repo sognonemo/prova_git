791cac6db64760d2247759a7e2765a72baac5b5a
odino <sognonemo@gmail.com>
2015-12-01 23:16:27 +0100

    Questo è un commento di prova

---
+++ usb/scheda_interna.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta charset="utf-8">
+  <title>USB Progetto Chiavette - Scheda Interna</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="stylesheet" href="css/main.css">
+  <!--[if lt IE 9]>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+  <!-- Specify the URI of your favicon in href attribute -->
+  <link rel="shortcut icon" href="">
+
+  <!-- The commented-out markup below is for iOS/Android bookmarking icons-->
+  <!--
+  <meta name="mobile-web-app-capable" content="yes">
+  <link rel="icon" sizes="196x196" href="">
+  <link rel="apple-touch-icon" sizes="152x152" href="">
+  -->
+
+  <!-- Reference non-render-blocking scripts here and use async attribute if possible
+  Example: <script src="" async></script>
+  -->
+</head>
+<body>
+<header role="banner">
+    <section>
+
+        <a class="brand">Site Title or Logo</a>
+        <div>
+            <a class="language"><img src="#" alt="Bandiera inglese">Bandiera inglese</a>
+        </div>
+    </section>
+    <nav role="navigation">
+      <ul class="navbar">
+        <li><a href="#">Usb Modelli</a></li>
+        <li><a href="#">Perché noi</a></li>
+        <li><a href="#">Siae</a></li>
+        <li><a href="#">Domande Frequenti</a></li>
+        <li><a href="#">Extra</a></li>
+        <li><a href="#">Contattaci</a></li>
+      </ul>
+    </nav>
+
+  </header>
+  <main role="main">
+    <!-- Messaggio Home -->
+    <section>
+        <div class="thumbs">
+          <a href="#"><img src="img/thumb_scheda.png" alt="#"></a>
+          <a href="#"><img src="img/thumb_scheda.png" alt="#"></a>
+          <a href="#"><img src="img/thumb_scheda.png" alt="#"></a>
+        </div>
+        <div class="focusprodotto">
+          <!-- Va Figure ? -->
+          <img src="img/chiave_scheda_prod.png" alt="thumb_3">
+          <h1>Elegante</h1>
+        </div>
+    </section>
+    <section class="plus">
+      <a href="#" alt=""><img src="img/orologio.png" alt=""><span>Spedizioni in Europa 24h/48h</span></a>
+        <a href="#" alt=""><img src="img/controllo_qualita.png" alt=""><span>Controllo Qualità</span></a>
+          <a href="#" alt=""><img src="img/garanzia.png" alt=""><span>1 anno di Garanzia</span></a>
+          <a href="#" alt=""><img src="img/prototipo.png" alt=""><span>Proposta grafica gratuita</span></a>
+    </section>
+    <section class="sotto">
+      <article>
+        <div>
+          <h1>Titolo</h1>
+          <p>
+            Chiave USB modello BY-U002 in materiale plastico. Gadget caratterizzato dalla forma rettangolare, questa chiavetta usb ha un peso di soli 8 grammi, dalle dimensioni generose per la stampa del logo, tecnologia Surface Mount Technology (SMT), che garantisce una robustezza a prova d'urto ed una maggiore resistenza nel tempo.
+La parte laterale della pendrive è semi trasparente e disponibile in diverse colorazioni e con l'accensione del led, un effetto di illuminazione diffusa lateralmente dà un tocco originale. Una grande capacità di memoria in meno di 7,35 cm.
+Generose le dimensioni di questa chiavetta usb per la personalizzazione: la superficie fronte e retro infatti è completamente personalizzabile con stampa serigrafica in quadricromia che permette l'utilizzo di colori brillanti.
+La pen drive usb personalizzabile è dotata inoltre di un foro funzionale al passaggio degli accessori. Questo modello di chiave usb ha la possibilità di essere corredate da alcuni optional, quali i cordini da collo integrati o agganciabili alle chiavette usb, i porta chiavi, gli anelli metallici.
+Alcuni esempi di stampa sulle nostre chiavette personalizzabil
+          </p>
+        </div>
+
+      <div>
+        <h1>Titolo</h1>
+        <p>
+          Chiave USB modello BY-U002 in materiale plastico. Gadget caratterizzato dalla forma rettangolare, questa chiavetta usb ha un peso di soli 8 grammi, dalle dimensioni generose per la stampa del logo, tecnologia Surface Mount Technology (SMT), che garantisce una robustezza a prova d'urto ed una maggiore resistenza nel tempo.
+La parte laterale della pendrive è semi trasparente e disponibile in diverse colorazioni e con l'accensione del led, un effetto di illuminazione diffusa lateralmente dà un tocco originale. Una grande capacità di memoria in meno di 7,35 cm.
+Generose le dimensioni di questa chiavetta usb per la personalizzazione: la superficie fronte e retro infatti è completamente personalizzabile con stampa serigrafica in quadricromia che permette l'utilizzo di colori brillanti.
+La pen drive usb personalizzabile è dotata inoltre di un foro funzionale al passaggio degli accessori. Questo modello di chiave usb ha la possibilità di essere corredate da alcuni optional, quali i cordini da collo integrati o agganciabili alle chiavette usb, i porta chiavi, gli anelli metallici.
+Alcuni esempi di stampa sulle nostre chiavette personalizzabil
+        </p>
+      </div>
+      </article>
+      <aside>
+        <h1>Preventivo</h1>
+        <form action="#" enctype="multipart/form-data" method="post" target="_self">
+          <div>
+            <label for="quantita">Quantità</label>
+            <select id="language_dropdown" name="quantita">
+          			<option value="50" >50</option>
+          			<option value="20" >20</option>
+  		      </select>
+          </div>
+          <div>
+            <label for="memoria">Memoria</label>
+            <select id="language_dropdown" name="memoria">
+          			<option value="1GB" >1GB</option>
+          			<option value="5GB" >5GB</option>
+  		      </select>
+          </div>
+          <hr>
+        </form>
+      </aside>
+    </section>
+
+
+      <!-- Fine Slide Home -->
+
+
+
+
+  </main><!-- End primary page content -->
+
+  <footer role="contentinfo">
+
+    <div>Page footer content</div>
+
+    <small>Copyright &copy; <time datetime="2013">2013</time></small>
+
+  </footer>
+  <!-- SCRIPTS -->
+  <!-- Example: <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script> -->
+</body>
+</html>
